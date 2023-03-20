cb82f852d455a108622b2bf472012b5e9df15342
Konstantin Lindner <hello@konte.se>
2023-03-20 16:05:53 -0600

    moce things around

--- src/server/README.md
+++ src/server/README.md
@@ -1,1 +1,3 @@
-# life-organizer BE
+# Backend
+
+The backend is based on Express for the rest API and Prisma for communicating with the DB. The DB itself is a CockroachDB instance.
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,14 +3,11 @@ import express from "express";
 
 const prisma = new PrismaClient();
 const app = express();
+
 app.use(express.json());
 
-(async () => {
-  try {
-    console.log(await prisma.widget.create({ data: {} }));
-  } catch (err) {
-    console.error("error executing query:", err);
-  } finally {
-    prisma.$disconnect();
-  }
-})();
+const server = app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
+);
