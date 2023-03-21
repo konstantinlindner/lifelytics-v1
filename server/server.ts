39c116d5bcc6b409ba7d827a6e544c6d1ee82784
Konstantin Lindner <hello@konte.se>
2023-03-20 23:03:39 -0600

    testing prisma

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/", (reg: Request, res: Response) => {
-  res.json({ message: "hello2test" });
+// get all users
+app.get("/", async (reg: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json({ users });
 });
+
+// create user
+app.post("/", (reg: Request, res: Response) => {});
+
+// delete a single user
+app.delete("/:user_id", (reg: Request, res: Response) => {});
 
 app.listen(3001);
