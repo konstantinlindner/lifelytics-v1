6d696b6b0e34de259c2176a2cb76760fa642bf3d
Konstantin Lindner <hello@konte.se>
2023-03-28 01:09:57 -0600

    fix colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,29 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "light-green": #b6d7a8,
-        "light-blue": #a4c2f4,
-        "light-yellow": #ffe599,
-        "light-red": #ea9999,
-        "light-purple": #b4a7d6,
-        "light-pink": #d5a6bd,
-      }
+        "light-green": "#b6d7a8",
+        "light-blue": "#a4c2f4",
+        "light-yellow": "#ffe599",
+        "light-red": "#ea9999",
+        "light-purple": "#b4a7d6",
+        "light-pink": "#d5a6bd",
+      },
     },
     fontFamily: {
       Roboto: ["Roboto"],
     },
     container: {
       center: true,
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-},
-}
+};
