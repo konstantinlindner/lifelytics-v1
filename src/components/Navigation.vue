71c46e1348d799c31f1f05ee9f3b5f94d37057c9
Konstantin Lindner <hello@konte.se>
2023-04-09 15:20:13 -0600

    some changes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,23 @@
 <script>
+import store from "../store/index.js";
+import { computed } from "vue";
 import { supabase } from "../supabase.js";
 import { useRouter } from "vue-router";
 export default {
   setup() {
     // get user from store
+    const user = computed(() => store.state.user);
 
     // setup ref to router
-    const router = useRouter;
+    const router = useRouter();
 
     // logout function
     const logout = async () => {
       await supabase.auth.signOut();
-      router.push("Home");
+      router.push({ name: "Home" });
     };
 
-    return { logout };
+    return { logout, user };
   },
 };
 </script>
@@ -28,20 +31,17 @@ export default {
         <div class="cursor-pointer select-none flex items-center gap-x-4">
           <img class="W-14" width="50" src="../assets/logo.png" />
           <h1 class="text-xl">
-            <span class="font-thin">life</span
-            ><span class="font-normal">lytics</span>
+            <span class="font-normal">life</span
+            ><span class="font-thin">lytics</span>
           </h1>
         </div></router-link
       >
       <ul class="flex flex-1 justify-end gap-x-10">
-        <router-link class="cursor-pointer" :to="{ name: 'Login' }"
-          >Login</router-link
+        <router-link v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }"
+          >Sign in</router-link
         >
-        <router-link class="cursor-pointer" :to="{ name: 'Register' }"
-          >Register</router-link
-        >
-        <li class="cursor-pointer">Profile</li>
-        <li @click="logout" class="cursor-pointer">Logout</li>
+        <li v-if="user" class="cursor-pointer">Profile</li>
+        <li v-if="user" @click="logout" class="cursor-pointer">Sign out</li>
       </ul>
     </nav>
   </header>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="mx-10 mt-10">
+    <h1 class="text-xl font-semibold">Welcome to lifelytics</h1>
+    <br />
+    <p>Please sign in to continue.</p>
   </div>
 </template>
 
