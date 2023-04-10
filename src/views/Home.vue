d0eebc538b5d7fc01b5da31a51e7731eedeec793
Konstantin Lindner <hello@konte.se>
2023-04-10 17:33:58 -0600

    Changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,37 @@
 <script>
+import store from "../store/index.js";
+import { computed } from "vue";
+import { supabase } from "../supabase.js";
 export default {
   name: "home",
   components: {},
   setup() {
-    // blabla
-    //blabla
+    const user = computed(() => store.state.user);
 
-    return {};
+    async function getUserData() {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+    }
+
+    getUserData();
+
+    return { user };
   },
 };
 </script>
 
 <template>
-  <div class="mx-10 my-10">
-    <h1 class="text-xl">Welcome to lifelytics</h1>
+  <div v-if="!user" class="mx-10 my-10">
+    <h1 class="text-xl">Welcome to lifelytics!</h1>
     <br />
+    <p>Please sign in to continue.</p>
+  </div>
+  <div v-else class="mx-10 my-10">
+    <h1 class="text-xl">
+      <span>Welcome back </span><span class="italic">{{ user.email }}</span>
+    </h1>
+    <br />
+    <p></p>
   </div>
 </template>
