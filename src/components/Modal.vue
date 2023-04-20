d09ed1e8c0d7c3da8385fa4ab654d08986051de9
Konstantin Lindner <hello@konte.se>
2023-04-20 04:16:52 -0600

    modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,9 @@
 <script>
+import { Icon } from "@iconify/vue";
+
 export default {
   name: "Modal",
+  components: { Icon },
   methods: {
     close() {
       this.$emit("close");
@@ -14,26 +17,31 @@ export default {
     <div
       class="fixed inset-0 bg-overlay-background flex justify-center items-center"
     >
-      <div class="bg-white shadow-md overflow-x-auto flex flex-col">
-        <header class="p-15 flex relative text-light-green justify-between">
-          <slot name="header"> This is the default title! </slot>
-          <button type="button" class="btn-close" @click="close">x</button>
-        </header>
+      <div class="bg-white shadow-lg rounded-md">
+        <div class="flex flex-col mx-2 my-2 w-80 h-40">
+          <header class="ml-auto">
+            <button type="button" class="" @click="close">
+              <Icon icon="mdi:close" />
+            </button>
+          </header>
 
-        <section class="relative px-20 py-10">
-          <slot name="body"> This is the default body! </slot>
-        </section>
+          <section class="mx-auto">
+            <slot name="body">This is a modal</slot>
+          </section>
 
-        <footer class="p-15 flex flex-col justify-end">
-          <slot name="footer"> This is the default footer! </slot>
-          <button
-            type="absolute top-0 right-0 text-sm p-10 cursor-pointer font-bold text-light-green"
-            class=""
-            @click="close"
-          >
-            Close Modal
-          </button>
-        </footer>
+          <footer class="flex flex-col mt-auto">
+            <li class="ml-auto list-none mx-3 my-2">
+              <div class="flex flex-row items-center">
+                <button
+                  class="bg-light-green duration-300 hover:bg-dark-light-green rounded-md px-2 py-1 flex flex-row items-center cursor-pointer"
+                  @click="close"
+                >
+                  <p>Confirm</p>
+                </button>
+              </div>
+            </li>
+          </footer>
+        </div>
       </div>
     </div>
   </transition>
