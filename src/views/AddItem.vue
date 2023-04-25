3e1ebc2453e4f4b0514f6c3e5a54e1f4f7095f0d
Konstantin Lindner <hello@konte.se>
2023-04-25 15:37:37 -0600

    fix shadow bug

--- src/views/AddItem.vue
+++ src/views/AddItem.vue
@@ -38,9 +38,9 @@ export default {
 
 <template>
   <div v-if="user" class="px-4 py-4 mx-auto">
-    <div class="min-w-max p-4 bg-light-light-grey rounded-md">
+    <div class="min-w-max p-4 bg-light-light-grey rounded-md shadow-md">
       <h1 class="text-xl mb-6 flex justify-center">Choose category</h1>
-      <div class="min-w-max grid gap-4 grid-cols-3 rounded-md shadow-lg">
+      <div class="min-w-max grid gap-4 grid-cols-3 rounded-md">
         <button
           class="bg-light-grey hover:bg-light-green duration-300 rounded-md w-40 h-40 cursor-pointer flex flex-col items-center justify-center"
           @click=""
